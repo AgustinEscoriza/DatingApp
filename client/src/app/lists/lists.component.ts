0dcc5678627cc6e8109b4a962d3075fd9b89d3fe
AgustinEscoriza <agusescoriza@outlook.es>
2023-04-01 18:06:04 -0300

    Routing, temas(UI) y tostadas

---
+++ client/src/app/lists/lists.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lists',
+  template: `
+    <p>
+      lists works!
+    </p>
+  `,
+  styleUrls: ['./lists.component.css']
+})
+export class ListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
